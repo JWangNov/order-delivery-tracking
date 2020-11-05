f6eef12e2595c7c3f3ee75366a2b2ed30881dd16
JWangNov <wangjialun8421@gmail.com>
2020-11-05 14:21:24 -0500

    Create OrderController.addOrder

--- src/main/java/com/jw/order/tracking/controller/OrderController.java
+++ src/main/java/com/jw/order/tracking/controller/OrderController.java
@@ -1,9 +1,30 @@
 package com.jw.order.tracking.controller;
 
+import com.jw.order.tracking.common.OrderStatus;
+import com.jw.order.tracking.pojo.Order;
+import com.jw.order.tracking.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @Slf4j
 @RestController
 public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping("add-order")
+    public String addOrder(Order order) {
+        // FIXME: for now, we simplify this part
+        //  once added an order, mark it as "shipped", and set the ship time as current time
+        order.setStatus(OrderStatus.SHIPPED)
+                .setOrderTime(new Date())
+                .setShipTime(new Date());
+        orderService.addOrder(order);
+        return "success adding the order";
+    }
 }

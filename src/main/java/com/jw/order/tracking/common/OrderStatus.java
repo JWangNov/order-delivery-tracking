565d18e4a75a5a81f7c56b5fbab27c1b091c4cf3
JWangNov <wangjialun8421@gmail.com>
2020-11-05 14:20:04 -0500

    Create OrderStatus.java

---
+++ src/main/java/com/jw/order/tracking/common/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.jw.order.tracking.common;
+
+public class OrderStatus {
+    public static final String PREPARING = "preparing for shipment";
+    public static final String SHIPPED = "shipped";
+    public static final String OUT_DELIVERY = "out for delivery";
+    public static final String DELIVERED = "delivered";
+}

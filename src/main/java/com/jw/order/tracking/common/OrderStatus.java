28c8d30099ad5db9c266d69264dedbd22de6fdaa
JWangNov <wangjialun8421@gmail.com>
2020-11-05 15:38:09 -0500

    Add javadoc for OrderStatus

--- src/main/java/com/jw/order/tracking/common/OrderStatus.java
+++ src/main/java/com/jw/order/tracking/common/OrderStatus.java
@@ -1,5 +1,12 @@
 package com.jw.order.tracking.common;
 
+import com.jw.order.tracking.pojo.Logistics;
+
+/**
+ * This only contains some key status of an order.
+ * <p>
+ * Generally, the order should use the latest {@link Logistics#getOperation()} as its status.
+ */
 public class OrderStatus {
     public static final String PREPARING = "preparing for shipment";
     public static final String SHIPPED = "shipped";
